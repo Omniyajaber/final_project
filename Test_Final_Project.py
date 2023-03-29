2f562db491d371d8c564611c6be02e8e4399c335
OmniyaJaber <jaberomniya@gmail.com>
2023-03-30 00:07:51 +0300

    jjj

---
+++ Test_Final_Project.py
@@ -0,0 +1,62 @@
+import pytest
+from main import *
+from selenium import webdriver
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    return driver
+
+@pytest.fixture
+def url():
+    return 'https://www.saucedemo.com/'
+#--------------------------negative login----------------------------------------
+
+class TestPy:
+    @pytest.mark.parametrize('username,password', [('username', 'password')])
+    def test_login_negative(self,driver, url,username,password):
+        open_site(driver,url)
+        expected = url
+        actual = login(driver, username, password)
+        assert actual == expected
+
+#_________________negative_test_______
+    @pytest.mark.parametrize('username,password', [('standard_user', 'secret_sauce')])
+    def test_login(self, driver, url, username, password):
+        open_site(driver, url)
+        expected = url
+        actual = login(driver, username, password)
+        assert actual != expected
+#-----------------------------negative test--------------------------
+#------------------------matching pictures---------------------------
+
+    @pytest.mark.parametrize('username,password', [('standard_user', 'secret_sauce')])
+    def test_image (self,driver,url,username,password):
+        expected =image2(driver, url, username, password)
+        actual = image1(driver, url, username, password)
+        assert actual != expected
+
+    @pytest.mark.parametrize('username,password', [('problem_user', 'secret_sauce')])
+    def test_image(self, driver, url, username, password):
+        expected = image2(driver, url, username, password)
+        actual = image1(driver, url, username, password)
+        assert actual == expected
+
+
+#------------------------matching pictures---------------------------
+#--------------API---------------------------------------------------
+class TestApi:
+
+    def test_status_code(self):
+        url = 'https://api.dictionaryapi.dev/api/v2/entries/en/human'
+        res = send_http_request(url)
+        assert check_status_code(res.status_code) == True
+
+    def test_check_word(self):
+        url = 'https://api.dictionaryapi.dev/api/v2/entries/en/human'
+        res = send_http_request(url)
+        assert check_status_code(res.status_code) == True
+
+
+
+
---
+++ main.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+
+
+def open_site(driver, url):
+    driver.get(url)
+    time.sleep(5)
+    return driver.current_url
+
+
+# _______________log in-Negative_________________________
+
+def login(driver, username, password):
+    name = driver.find_element(By.CSS_SELECTOR, '#user-name')
+    name.send_keys(username)
+    time.sleep(3)
+    passwordn = driver.find_element(By.CSS_SELECTOR, '#password')
+    passwordn.send_keys(password)
+    time.sleep(3)
+    button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+    button.click()
+    time.sleep(3)
+    return driver.current_url
+
+
+# _______________log in-Negative______________________________
+
+
+# ___________________failed_test________________________________
+
+
+
+# ___________________failed_test________________________________
+
+# ______________________matching_pictures_test__________________
+
+
+def image1(driver,url,username,password):
+    open_site(driver, url)
+    login(driver, username, password)
+    image3 = driver.find_element(By.CSS_SELECTOR, '#item_3_img_link')
+    return image3.get_attribute('src')
+
+def image2(driver,url,username,password):
+    open_site(driver,url)
+    login(driver,username, password)
+    image3 = driver.find_element(By.CSS_SELECTOR, '#item_1_img_link')
+    return image3.get_attribute('src')
+# _________________API___________
+
+def send_http_request(url):
+    res = requests.get(url)
+    return res
+
+
+def check_status_code(code):
+    if 200 <= code < 399:
+        print(f"success -->  {code}")
+        return True
+    else:
+        print('error')
+        return False
+
+
+def check_word():
+    def check_word(url):
+        res = requests.get(url)
+        word = res.json()[0]
+
+        if 'human' in url:
+            print(word['human'])
+            return True
+        else:
+            print('error')
+            return False
